48e9f4e0f81286a8d8cf9f13a62e76e12640e014
Rane Wallin <github@blasph.me>
2020-07-11 13:27:07 -0700

    Suppressing console output in production

---
+++ assets/log.js
@@ -0,0 +1,25 @@
+console.info(`
+************************************************
+*                                              *
+*      This proxy was made by Rane Wallin.     *
+*      It pulls in 8 services into a single    *
+*      view that is a clone of the Humble      *
+*      Bundle store's item page.               *
+*                                              *
+*      Rane also wrote the description         *
+*      service and also the related            *
+*      content section.                        *
+*                                              *
+*      If you would like more information      *
+*      email Rane at rane@blasph.me            *
+*                                              *
+************************************************
+
+
+`);
+console.log = function () {};
+console.error = function () {};
+console.warn = function () {};
+console.assert = function () {};
+console.info = function () {};
+console.trace = function () {};
