7a5ef5aa844355d931903f84521c945f37c580ab
Krissy Do <krissytdo@hotmail.com>
2020-09-19 21:40:57 -0700

    setup eslint

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
-const request = require("superagent");
+// const request = require("superagent");
 const cors = require("cors");
-const pug = require("pug");
-const fetch = require("node-fetch");
+// const pug = require("pug");
+// const fetch = require("node-fetch");
 const path = require("path");
 
 const app = express();
